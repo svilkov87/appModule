8ee371648df85ae832ffb9dbbfa769130d5e6db3
Сергей Вилков <vilkov@reg.ru>
2018-06-06 22:58:26 +0300

    Horizontal position

--- app/libs/main-min.js
+++ app/libs/main-min.js
@@ -1,7 +1,56 @@
 'use strict';
 
-var oneTest = 'one';
+var slider      = document.getElementById( UISlider.sliderId );
+var sliderWidth = slider.offsetWidth;
+var sliderLists = document.querySelectorAll("#" + UISlider.sliderId + " > li");
+// var images      = document.querySelectorAll("" + sliderLists + " > img");
 
-    if (oneTest === 'one') {
-        console.log('wow');
+    // console.log( sliderLists );
+    // console.log( sliderWidth );
+    // console.log( images );
+
+
+
+
+if ( UISlider.type == 'horizontal') {
+
+    // var test = 'display:flex';
+
+    // slider.style = test;
+    slider.className = 'b-ui_horizontal';
+
+    for( var i = 0; i < sliderLists.length; i++ ) {
+
+          sliderLists[i].style.width = sliderWidth + 'px';
     }
+
+}
+
+function styles( element ) {
+
+    var wrapper       = document.createElement('div');
+    var mainWrapper   = document.createElement('div');
+    var parent        = element.parentNode;
+
+    parent.insertBefore( wrapper, element );
+    wrapper.appendChild( element );
+
+    mainWrapper.className = "b-ui-wrapper";
+    wrapper.className     = "b-ui-viewport";
+
+    var parentWrapper = wrapper.parentNode;
+    var uiWrapper     = document.getElementsByClassName( 'b-ui-wrapper' )[0];
+
+    parentWrapper.insertBefore( mainWrapper, uiWrapper );
+    mainWrapper.appendChild( wrapper );
+
+
+
+}
+
+styles( slider );
+
+function createElement( element ) {
+
+}
+
