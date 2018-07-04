c663e710e80eb519a044c79abab98715b28e3fec
Сергей Вилков <vilkov@reg.ru>
2018-07-04 23:24:46 +0300

    Pagers

--- app/libs/main-min.js
+++ app/libs/main-min.js
@@ -1,57 +1,74 @@
 'use strict';
 
-var slider      = document.getElementById( UISlider.sliderId );
-var sliderWidth = slider.offsetWidth;
-var sliderLists = document.querySelectorAll("#" + UISlider.sliderId + " > li");
+const slider      = document.getElementById( UISlider.sliderId );
+const sliderWidth = slider.offsetWidth;
+const sliderLists = document.querySelectorAll("#" + UISlider.sliderId + " > li");
+const sliderImg   = document.querySelectorAll("#" + UISlider.sliderId + " > li > img");
 
-    // console.log( sliderLists );
-    // console.log( sliderWidth );
-    // console.log( images );
+function settings( element ) {
 
-
-// if ( UISlider.type == 'row') {
-
-//     slider.classList.add('b-ui_horizontal');
-
-//     // for( var i = 0; i < sliderLists.length; i++ ) {
-
-//     //     sliderLists[i].style.width = sliderWidth + 'px';
-
-//     // }
-
-// }
-
-function styles( element ) {
-
-    var wrapper     = document.createElement('div');
-    var mainWrapper = document.createElement('div');
-    var prevArrow   = document.createElement('span');
-    var nextArrow   = document.createElement('span');
-    var parent      = element.parentNode;
+    const wrapper     = document.createElement('div');
+    const mainWrapper = document.createElement('div');
+    const pagerWrapper = document.createElement('div');
+    const prevArrow   = document.createElement('span');
+    const nextArrow   = document.createElement('span');
+    const parent      = element.parentNode;
 
     parent.insertBefore( wrapper, element );
     wrapper.appendChild( element );
 
-    mainWrapper.className = "b-ui-wrapper";
-    wrapper.className     = "b-ui-viewport";
-    prevArrow.className   = "b-ui-prev";
-    nextArrow.className   = "b-ui-next";
+    mainWrapper.className  = "b-ui-wrapper";
+    wrapper.className      = "b-ui-viewport";
+    pagerWrapper.className = "b-ui-pager-wrapper";
+    prevArrow.className    = "b-ui-prev";
+    nextArrow.className    = "b-ui-next";
 
-    var parentWrapper = wrapper.parentNode;
-    var uiWrapper     = document.getElementsByClassName( 'b-ui-wrapper' )[0];
+    const parentWrapper = wrapper.parentNode;
+    const uiWrapper     = document.getElementsByClassName( 'b-ui-wrapper' )[0];
 
     parentWrapper.insertBefore( mainWrapper, uiWrapper );
     mainWrapper.appendChild( wrapper );
+    mainWrapper.appendChild( pagerWrapper );
 
-    if ( UISlider.arrows ) {
-        mainWrapper.appendChild( prevArrow );
-        mainWrapper.appendChild( nextArrow );
+    for (var i = 0; i < sliderImg.length; i++) {
+
+        pagerWrapper.innerHTML += '<div class="b-ui-pager" data-index-pager="'+ i +'"></div>';
+        sliderImg[i].setAttribute('data-index-slider', ''+ i + '');
+
     }
+    if ( UISlider.arrows ) {
+        wrapper.appendChild( prevArrow );
+        wrapper.appendChild( nextArrow );
+    }
+
+    let sliderWidthArray = [];
+    sliderWidthArray.push(sliderWidth);
+    sliderWidthArray = [sliderWidthArray.pop()];
+
+    window.addEventListener('resize', function() {
+
+        sliderWidthArray = [];
+
+        const elemWidth = slider.offsetWidth;
+
+        sliderWidthArray.push(elemWidth);
+        sliderWidthArray = [sliderWidthArray.pop()];
+
+    } );
+
+    nextArrow.addEventListener('click', function () {
+
+        slider.style.transform = 'translate3d(' + '-' + sliderWidthArray[0] + 'px, 0, 0)';
+
+    } );
+
+    prevArrow.addEventListener('click', function () {
+
+        slider.style.transform = 'translate3d(' + sliderWidthArray[0] / 2 + 'px, 0, 0)';
+
+    } );
+
 }
 
-styles( slider );
-
-function createElement( element ) {
-
-}
+settings( slider );
 
