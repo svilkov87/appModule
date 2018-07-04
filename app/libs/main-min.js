39648712ab81b75b7bee2a91fdb7b4fa5f4431e9
Сергей Вилков <vilkov@reg.ru>
2018-07-04 08:33:06 +0300

    Arrows

--- app/libs/main-min.js
+++ app/libs/main-min.js
@@ -3,40 +3,39 @@
 var slider      = document.getElementById( UISlider.sliderId );
 var sliderWidth = slider.offsetWidth;
 var sliderLists = document.querySelectorAll("#" + UISlider.sliderId + " > li");
-// var images      = document.querySelectorAll("" + sliderLists + " > img");
 
     // console.log( sliderLists );
     // console.log( sliderWidth );
     // console.log( images );
 
 
+// if ( UISlider.type == 'row') {
 
+//     slider.classList.add('b-ui_horizontal');
 
-if ( UISlider.type == 'horizontal') {
+//     // for( var i = 0; i < sliderLists.length; i++ ) {
 
-    // var test = 'display:flex';
+//     //     sliderLists[i].style.width = sliderWidth + 'px';
 
-    // slider.style = test;
-    slider.className = 'b-ui_horizontal';
+//     // }
 
-    for( var i = 0; i < sliderLists.length; i++ ) {
-
-          sliderLists[i].style.width = sliderWidth + 'px';
-    }
-
-}
+// }
 
 function styles( element ) {
 
-    var wrapper       = document.createElement('div');
-    var mainWrapper   = document.createElement('div');
-    var parent        = element.parentNode;
+    var wrapper     = document.createElement('div');
+    var mainWrapper = document.createElement('div');
+    var prevArrow   = document.createElement('span');
+    var nextArrow   = document.createElement('span');
+    var parent      = element.parentNode;
 
     parent.insertBefore( wrapper, element );
     wrapper.appendChild( element );
 
     mainWrapper.className = "b-ui-wrapper";
     wrapper.className     = "b-ui-viewport";
+    prevArrow.className   = "b-ui-prev";
+    nextArrow.className   = "b-ui-next";
 
     var parentWrapper = wrapper.parentNode;
     var uiWrapper     = document.getElementsByClassName( 'b-ui-wrapper' )[0];
@@ -44,8 +43,10 @@ function styles( element ) {
     parentWrapper.insertBefore( mainWrapper, uiWrapper );
     mainWrapper.appendChild( wrapper );
 
-
-
+    if ( UISlider.arrows ) {
+        mainWrapper.appendChild( prevArrow );
+        mainWrapper.appendChild( nextArrow );
+    }
 }
 
 styles( slider );
