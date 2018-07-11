62c18ba74189cea2b7aff58ca143a7a48c2e2034
Сергей Вилков <vilkov@reg.ru>
2018-07-11 21:21:10 +0300

    All methods

--- app/libs/main-min.js
+++ app/libs/main-min.js
@@ -2,12 +2,14 @@
 
 var appModule = ( function() {
 
-    var summ       = 0;
-    var goods      = [];
-    var menubutton = document.getElementsByClassName('b-phone-screen__touch-menu')[0];
-    var menu       = document.getElementsByClassName('b-phone-screen__menu')[0];
-    var menuItems  = document.getElementsByClassName('b-phone-screen__list-item');
-    var article    = document.getElementsByClassName('b-phone-screen__content')[0];
+    var menubutton   = document.getElementsByClassName('b-phone-screen__touch-menu')[0];
+    var menu         = document.getElementsByClassName('b-phone-screen__menu')[0];
+    var menuItems    = document.getElementsByClassName('b-phone-screen__list-item');
+    var article      = document.getElementsByClassName('b-phone-screen__content')[0];
+    var products     = document.getElementsByClassName('b-phone-screen__product');
+    var total        = document.getElementsByClassName('b-phone-screen__total')[0];
+    var basket       = document.getElementsByClassName('b-phone-screen__basket')[0];
+    var productArray = [];
 
     return {
 
@@ -34,28 +36,47 @@ var appModule = ( function() {
             });
 
         },
-        addProduct: function( product ) {
-            summ += product.price;
-            goods.push( product );
-        },
-        printProduct: function () {
-            for (var i = 0; i < goods.length; i++) {
+        activeBasket: function() {
 
-                console.log( goods[i].name, goods[i].price );
+            total.addEventListener('click', function(){
+                basket.classList.toggle('b-phone-screen__basket-active');
+                article.classList.toggle('b-phone-screen__content-move');
+                menubutton.classList.toggle('b-phone-screen__touch-menu_state_disabled');
+            });
+
+        },
+        chooseProduct: function () {
+
+            var count            = 0;
+            var totalPrice       = document.createElement('div');
+            totalPrice.className = "b-phone-screen__basket-price";
+            basket.appendChild( totalPrice );
+
+            for (var i = 0; i < products.length; i++) {
+
+                products[i].addEventListener('click', function(){
+
+                    var totalString = document.createElement('div');
+                    totalString.className = "b-phone-screen__basket-total";
+                    totalString.setAttribute('data-total-price', this.getAttribute('data-price') );
+                    basket.appendChild( totalString );
+
+                    count++;
+
+                    totalString.innerHTML = this.firstElementChild.innerHTML + ', Стоимость: ' + this.getAttribute('data-price') + '$';
+                    totalPrice.innerHTML = Number(this.getAttribute('data-price')) + Number(totalPrice.innerHTML);
+                    total.innerHTML = count;
+                    total.classList.add('b-phone-screen__total-active');
+
+                });
 
             }
         }
-
     };
 
 }());
 
-var salt = {
-    name: 'Salt',
-    price: '20'
-};
-
-appModule.addProduct(salt);
-appModule.printProduct();
+appModule.chooseProduct();
 appModule.activeMenu();
+appModule.activeBasket();
 appModule.scrollSection();
